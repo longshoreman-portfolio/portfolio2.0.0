e1397a6a79f35cdd429464773a7ff17aeff5fd42
zacksailor <zakarabenjawed@gmail.com>
2022-01-10 14:54:18 +0100

    create rounded cube

--- main.js
+++ main.js
@@ -30,8 +30,46 @@ scene.add(torus)
 const geometry2 = new THREE.SphereGeometry(3.5, 30, 30)
 const material2 = new THREE.MeshStandardMaterial({color:0x4d0099})
 const sphere = new THREE.Mesh(geometry2, material2)
-sphere.position.set(0, 0, 0)
+sphere.position.set(30, 10, -20)
 scene.add(sphere)
+
+// cube 
+const geometry3 = new  boxWithRoundedEdges(10, 10, 10, 2, 6, 2)
+const material3 = new THREE.MeshStandardMaterial({color:0x111111})
+const cube = new THREE.Mesh(geometry3, material3)
+cube.position.set(15, 5, -15)
+
+cube.rotation.x = 10
+cube.rotation.y = 10
+cube.rotation.z = 10
+
+scene.add(cube)
+
+
+function boxWithRoundedEdges(width, height, depth, radius0, smoothness) {
+    let shape = new THREE.Shape();
+    let eps = 0.00001;
+    let radius = radius0 - eps;
+    shape.absarc(eps, eps, eps, -Math.PI / 2, -Math.PI, true);
+    shape.absarc(eps, height - radius * 2, eps, Math.PI, Math.PI / 2, true);
+    shape.absarc(width - radius * 2, height - radius * 2, eps, Math.PI / 2, 0, true);
+    shape.absarc(width - radius * 2, eps, eps, 0, -Math.PI / 2, true);
+    let geometry = new THREE.ExtrudeBufferGeometry(shape, {
+        depth: depth - radius0 * 2,
+        bevelEnabled: true,
+        bevelSegments: smoothness * 2,
+        steps: 1,
+        bevelSize: radius,
+        bevelThickness: radius0,
+        curveSegments: smoothness
+    });
+ 
+    geometry.center();
+ 
+    return geometry;
+}
+
+
 
 
 
