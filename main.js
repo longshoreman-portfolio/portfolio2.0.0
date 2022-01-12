f3980de94cf15f3d626f16794451d22e80c87785
zacksailor <zakarabenjawed@gmail.com>
2022-01-12 17:46:14 +0100

    refactor

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import './style.css'
 import * as THREE from 'three'
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Cylindrical } from 'three'
 
 const scene = new THREE.Scene()
 
+/** Camera */
 const camera = new THREE.OrthographicCamera( window.innerWidth / - 20, window.innerWidth / 20, window.innerHeight / 20, window.innerHeight / - 20, 1, 1000 )
 
 const renderer = new THREE.WebGLRenderer({
@@ -16,36 +18,43 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(50)
 
+/** Background */
 scene.background = new THREE.Color( 0x181822 )
 
+/** Interaction with shapes */ 
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableZoom = false;
 
-// Torus shape
-const geometry = new THREE.TorusGeometry(10, 2.8, 30, 200)
-const material = new THREE.MeshStandardMaterial({color:0x111111})
-const torus = new THREE.Mesh(geometry, material)
+/** Lights */
+const pointLightWhite = new THREE.PointLight(0xffffff)
+pointLightWhite.position.set(10, 10, 10)
+
+const pointLightGreen = new THREE.PointLight(0x00e68a)
+pointLightGreen.position.set(20, -9, 0)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(ambientLight, pointLightWhite)
+
+/** Materials */
+const darkMaterial = new THREE.MeshStandardMaterial({color:0x111111})
+const purpleMaterial = new THREE.MeshStandardMaterial({color:0x4d0099})
+const greenMaterial = new THREE.MeshStandardMaterial({color:0x00995c, side: THREE.DoubleSide})
+
+
+
+/** Torus shape */ 
+const geometryTorus = new THREE.TorusGeometry(10, 2.8, 30, 200)
+const torus = new THREE.Mesh(geometryTorus, darkMaterial)
 torus.position.set(20, -9, 0)
 scene.add(torus)
 
-// sphere
-const geometry2 = new THREE.SphereGeometry(3.5, 30, 30)
-const material2 = new THREE.MeshStandardMaterial({color:0x4d0099})
-const sphere = new THREE.Mesh(geometry2, material2)
+/** Sphere Shape */
+const geometrySphere = new THREE.SphereGeometry(3.5, 30, 30)
+const sphere = new THREE.Mesh(geometrySphere, purpleMaterial)
 sphere.position.set(30, 10, -20)
 scene.add(sphere)
 
-// cube 
-const geometry3 = new  boxWithRoundedEdges(10, 10, 10, 2, 6, 2)
-const material3 = new THREE.MeshStandardMaterial({color:0x111111})
-const cube = new THREE.Mesh(geometry3, material3)
-cube.position.set(15, 5, -15)
-
-cube.rotation.x = 10
-cube.rotation.y = 10
-cube.rotation.z = 10
-
-scene.add(cube)
-
-
+/** Cube helper */ 
 function boxWithRoundedEdges(width, height, depth, radius0, smoothness) {
     let shape = new THREE.Shape();
     let eps = 0.00001;
@@ -69,46 +78,18 @@ function boxWithRoundedEdges(width, height, depth, radius0, smoothness) {
     return geometry;
 }
 
+/** Cube shape */  
+const geometryCube = new  boxWithRoundedEdges(10, 10, 10, 2, 6, 2)
+const cube = new THREE.Mesh(geometryCube, darkMaterial)
+cube.position.set(15, 5, -15)
 
+cube.rotation.x = 10
+cube.rotation.y = 10
+cube.rotation.z = 10
 
-/* to be tested more
-var light = new THREE.DirectionalLight(0xefefff, 1.5);
-light.position.set(1, 1, 1).normalize();
-scene.add(light);
+scene.add(cube)
 
-var light = new THREE.DirectionalLight(0xffefef, 1.5);
-light.position.set(-1, -1, -1).normalize();
-scene.add(light);
-*/
-
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(10, 10, 10)
-
-const ambientLight = new THREE.AmbientLight(0xffffff)
-scene.add(ambientLight, pointLight)
-
-
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableZoom = false;
-
-function animateTorus() {
-    requestAnimationFrame(animateTorus)
-
-    torus.rotation.x += 0.01
-    torus.rotation.y += 0.005
-    torus.rotation.z += 0.01
-
-    controls.update()
-
-    renderer.render(scene, camera)
-}
-animateTorus()
-
-
-// add other light sources with diff renet color 
-// add more shapes
-// animate each shape alone 
-
+/** Cylinder helper */ 
 function cylinderWithroundedendge(radius, height, curve, smoothness) {
     const points = [];
 
@@ -127,11 +108,30 @@ function cylinderWithroundedendge(radius, height, curve, smoothness) {
     return points
 }
 
-const points = cylinderWithroundedendge(4,10,1,10)
-console.log(points)
-const geometry6 = new THREE.LatheGeometry(  points ,50);
-const material6 = new THREE.MeshStandardMaterial( { color: 0x111111 , side: THREE.DoubleSide});
-const lathe = new THREE.Mesh( geometry6, material6 );
+/** Cylinder shpe */
+const points = cylinderWithroundedendge(4,9,1,10)
+const geometryCylinder = new THREE.LatheGeometry(  points ,50)
+const lathe = new THREE.Mesh( geometryCylinder, greenMaterial )
 lathe.position.set(20, -9, 0)
 lathe.scale.set(1, 1, 1);
 scene.add( lathe );
+
+
+/** Animation */ 
+function animateTorus() {
+    requestAnimationFrame(animateTorus)
+
+    torus.rotation.x += 0.01
+    torus.rotation.y += 0.005
+    torus.rotation.z += 0.01
+
+    controls.update()
+
+    renderer.render(scene, camera)
+}
+animateTorus()
+
+
+// add other light sources with diff renet color 
+// add more shapes
+// animate each shape alone 
