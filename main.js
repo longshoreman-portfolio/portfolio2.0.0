5cf38216519b69df26d9346852803d9445903524
zacksailor <zakarabenjawed@gmail.com>
2022-06-01 16:22:58 +0100

    remove old code

--- main.js
+++ main.js
@@ -272,40 +272,6 @@ controls.enableZoom = false;
 
 
 
-
-getDownloadURL(starsRef)
-
-  .then((url) => {
-    // Insert url into an <img> tag to "download"
-    gtlfLoadHelper(loader, url)
-  })
-
-
-  .catch((error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/object-not-found':
-        // File doesn't exist
-        console.error('File does not exist')
-        break;
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        console.error('User does not have permission to access the object')
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        console.error('User canceled the upload')
-        break;
-
-      // ...
-      case 'storage/unknown':
-        // Unknown error occurred, inspect the server response
-        console.error('Unknown error occurred, inspect the server response')
-        break;
-    }
-  })
-
     
 
 /** Background */
